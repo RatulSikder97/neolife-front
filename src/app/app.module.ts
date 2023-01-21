53b46c2ed2b166283a4dcd077e53e6ee3b00913c
ratul.sikder <ratulsikder104@gmail.com>
2023-01-21 16:20:46 +0600

    define components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskDetailsComponent } from './components/task-details/task-details.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TaskDetailsComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
