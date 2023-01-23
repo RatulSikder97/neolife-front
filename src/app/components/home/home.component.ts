06cdc9e1e50117e629431c1b8376d7da3d013763
ratul.sikder <ratulsikder104@gmail.com>
2023-01-23 16:57:15 +0600

    data render

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  data;
+  length;
+  ngOnInit(): void {
+    window["HomeComponent"] = this;
+    this.data = JSON.parse(window["ANDROID"].renderAllTask());
+  }
+  showTask() {
+    this.length = this.data.length;
+  }
 
+  showArray(arr) {
+    alert(arr);
+  }
 }
